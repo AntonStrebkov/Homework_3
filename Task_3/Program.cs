0de58ab8e75a1c0033a0c0b6080b516b096e43b5
AntonStrebkov <wancler987@gmail.com>
2022-08-13 23:14:04 +0700

    add homework#3

---
+++ Task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 23
+
+//Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void Cube(int num)
+
+{
+    Console.Write($"{num} -> ");
+    int index = 1;
+    while(index < num)
+    {
+        Console.Write($"{Math.Pow(index, 3)}, ");
+        index++;
+    }
+    Console.Write(Math.Pow(num, 3));
+    Console.WriteLine();
+}
+
+Cube(3);
+Cube(5);
+Cube(9);
